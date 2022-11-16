24be8a251ba1c85d329e519d7204e8152184edac
RAGHUL R <118167519+Raghulrook@users.noreply.github.com>
2022-11-16 20:01:22 +0530

    Update Operator_&_Numerical.py

--- Operator_&_Numerical.py
+++ Operator_&_Numerical.py
@@ -1,7 +1,7 @@
 # Bid-Data-Prep
 # From Scratch To The Professional
 
-# now we see about the numerical operator that are used in python
+# the numerical operator that are used in python
 + for Addition
 - for Subraction
 * for Multiplucation
@@ -26,13 +26,13 @@ print("Modulus of Dick and Pussy",Dick%Pussy) # Modulus
 print("Power Multiplication of Dick and Pussy",Dick**Pussy) # Power Multiplication 
 print("Intiger Division of Dick and Pussy",Dick//Pussy) # Intiger Division
 
-# Arithimatic for words like in Practically 
+# Arithimatic operators for words like in Practically 
 Dick="RO"
 Pussy="OK"
 
-print("Addition of Dick and Pussy",Dick+Pussy) # Addition
+print("Addition of Dick and Pussy using words",Dick+Pussy) # Addition
 
-# Rather Addtion other operators are not working 
+Rather Addtion other operators are not working 
 #print("Subraction of Dick and Pussy",Dick-Pussy) # Subraction
 #print("Multiplucation of Dick and Pussy",Dick*Pussy) # Multiplucation
 #print("Float Division of Dick and Pussy",Dick/Pussy) # Float Division
@@ -40,3 +40,32 @@ print("Addition of Dick and Pussy",Dick+Pussy) # Addition
 # Bonus 
 #print("Power Multiplication of Dick and Pussy",Dick**Pussy) # Power Multiplication 
 #print("Intiger Division of Dick and Pussy",Dick//Pussy) # Intiger Division
+
+# Combination of Numaric and Variable like in Practically 
+Dick="ROOK"
+Pussy=10
+ 
+print("Multiplying the Combination of Numaric and Variable-Dick and Pussy",Dick*Pussy) # Multiplucation
+
+Rather Multiplucation other operators are not working 
+#print("Addition of Dick and Pussy",Dick+Pussy) # Addition
+#print("Subraction of Dick and Pussy",Dick-Pussy) # Subraction
+#print("Float Division of Dick and Pussy",Dick/Pussy) # Float Division
+#print("Modulus of Dick and Pussy",Dick%Pussy) # Modulus 
+
+# the Assignment operators that are used in python like they are assinging the value using some shortcuts
+=  , x = 5
++= , x += 5 -> x = x + 5
+-= , x -= 5 -> x = x - 5
+*= , x *= 5 -> x = x * 5
+/= , x /= 5 -> x = x / 5
+//= , x //= 5 -> x = x // 5
+%= , x %= 5 -> x = x % 5
+
+# Comparison Operators ( we compare two operand values)
+== , Equals to condition , x == y
+!= , Not Equals to condition , x != y
+> , Greater than condition , x > y
+< , Less than condition , x < y
+>= , Greater than and Equals to condition , x >= y
+<= , Less than and Equals to condition , x <= y
