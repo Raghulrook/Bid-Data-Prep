c3a613359e70c7437eb213305e4663e1d21109e6
RAGHUL R <118167519+Raghulrook@users.noreply.github.com>
2022-11-16 19:34:56 +0530

    Update and rename Operator_&_Numerical to Operator_&_Numerical.py

---
+++ Operator_&_Numerical.py
@@ -0,0 +1,27 @@
+# Bid-Data-Prep
+# From Scratch To The Professional
+
+# now we see about the numerical operator that are used in python
++ for Addition
+- for Subraction
+* for Multiplucation
+/ for Float Division
+% for Modulus
+ 
+# Bonus 
+** for Power Multiplication
+// for Intiger Division
+ 
+# like in Practically 
+Dick=5
+Pussy=10
+ 
+print("Addition of Dick and Pussy",Dick+Pussy) # Addition
+print("Subraction of Dick and Pussy",Dick-Pussy) # Subraction
+print("Multiplucation of Dick and Pussy",Dick*Pussy) # Multiplucation
+print("Float Division of Dick and Pussy",Dick/Pussy) # Float Division
+print("Modulus of Dick and Pussy",Dick%Pussy) # Modulus 
+
+# Bonus 
+print("Power Multiplication of Dick and Pussy",Dick**Pussy) # Power Multiplication 
+print("Intiger Division of Dick and Pussy",Dick//Pussy) # Intiger Division
