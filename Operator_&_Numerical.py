4844d40037092fc23272809ebc60c8bfcc1c282e
RAGHUL R <118167519+Raghulrook@users.noreply.github.com>
2022-11-16 20:58:01 +0530

    Update Operator_&_Numerical.py

--- Operator_&_Numerical.py
+++ Operator_&_Numerical.py
@@ -58,14 +58,73 @@ Rather Multiplucation other operators are not working
 += , x += 5 -> x = x + 5
 -= , x -= 5 -> x = x - 5
 *= , x *= 5 -> x = x * 5
+**= , x *= 5 -> x = x ** 5
 /= , x /= 5 -> x = x / 5
 //= , x //= 5 -> x = x // 5
 %= , x %= 5 -> x = x % 5
 
-# Comparison Operators ( we compare two operand values)
+# Assignment operators Usage in Practically 
+A=2
+B=2
+C=2
+D=2
+E=2
+F=2
+G=2
+A += 5 
+B -= 5 
+C *= 5 
+D /= 5 
+E **= 5 
+F //= 5 
+G %= 5 
+print("A = ",A)
+print("B = ",B)
+print("C = ",C)
+print("D = ",D)
+print("E = ",E)
+print("F = ",F)
+print("G = ",G)
+
+# Comparison Operators ( we compare two operand values) it appears like in boolean fnction neither True or False
 == , Equals to condition , x == y
 != , Not Equals to condition , x != y
 > , Greater than condition , x > y
 < , Less than condition , x < y
 >= , Greater than and Equals to condition , x >= y
 <= , Less than and Equals to condition , x <= y
+
+# Comparison operators Usage in Practically 
+a = 10
+b = 5
+print("Result of a == b,",a == b)
+print("Result of a != b,", a != b)
+print("Result of a > b,", a > b)
+print("Result of a < b,", a < b)
+print("Result of a >= b,", a >= b)
+print("Result of a <= b,", a <= b)
+
+# logical operators in Python ( Logical check will happen for expression result)
+and -> Returns true if both statements are true 
+or -> Returns true if one of statements are true
+not -> Reverse the result, returns false if the result is true
+
+# logical operators Usage in Practically 
+m = 10
+n = 8
+o = 9
+print("m = 10, n = 8, o = 9")
+print("m>o And n<o Result " , m>o and n<o) # False and True -> False
+print("m>o Or n<o Result " , m>o or n<o) # False or True -> 
+print("Not (m>20 And n<10) Result " , not(m>o and n<o)) # not(False and True) -> not(False) -> True
+print("Not (m>o Or n<o) Result " , not(m>o or n<o)) # not(False or True) -> not(True) -> Flase
+
+# logical operators Usage in Practically by user input
+M=int(input("Enter the input for M",))
+O=int(input("Enter the input for O",))
+N=int(input("Enter the input for N",))
+
+print("M>O And N<O Result " , M>O and N<O) # False and True -> False
+print("M>O Or N<O Result " , M>O or N<O) # False or True -> True
+print("Not (M>O And N<O) Result " , not(M>O and N<O)) # not(False and True) -> not(False) -> True
+print("Not (M>O Or N<O) Result " , not(M>O or N<O)) # not(False or True) -> not(True) -> False
