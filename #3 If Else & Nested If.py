96146730ad1309f6d7ce13da1938261b052df52f
RAGHUL R <118167519+Raghulrook@users.noreply.github.com>
2022-11-22 11:06:33 +0530

    Update #3 If Else & Nested If.py

--- #3 If Else & Nested If.py
+++ #3 If Else & Nested If.py
@@ -1,1 +1,36 @@
+# Bid-Data-Prep
+# From Scratch To The Professional
 
+# See how the If-Else is Works
+Adam = 15
+Eval = 30
+if (Adam==Eval):
+  print("Adam and Eval are have same things")
+else:
+  print("Adam and Eval are not have same things")
+  
+# See how the If-Else is Works on Words
+Adam = 'on edge'
+Eval = 'on edge'
+if (Adam==Eval):
+  print("Adam and Eval are have same things")
+else:
+  print("Adam and Eval are not have same things")
+  
+# See how the If-Else is Works on combination of Words and Numbers by user input
+Adam = input("Enter a things that own for Adam") 
+Eval = input("Enter a things that own for Eval") 
+if (Adam==Eval):
+  print("Adam and Eval are have same things")
+else:
+  print("Adam and Eval are not have same things")
+
+# See how the Nested If-Else is Works
+Good Knight ,Mortein ,All Out = format( 12, 10, 13)
+Mosquito Repellent = Good Knight ,Mortein ,All Out
+if (Mosquito Repellent>12):
+  print("Mosquito Repellent that works for more than needed {}".format(Good Knight ,Mortein ,All Out ))
+elif (Mosquito Repellent<10 and Mosquito Repellent<=12):
+   print("Mosquito Repellent that works for more than needed {}".format(Good Knight ,Mortein ,All Out ))
+else:
+  print("Mosquito Repellent that works for more than needed {}".format(Good Knight ,Mortein ,All Out ))
