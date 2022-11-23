52f4b5d0a400c5083f24fca28517e2e5cfac0fa4
RAGHUL R <118167519+Raghulrook@users.noreply.github.com>
2022-11-23 23:13:42 +0530

    Update #5 While Loop.py

--- #5 While Loop.py
+++ #5 While Loop.py
@@ -1,1 +1,26 @@
+# Syntax
+# while exp:
+#     statements
 
+# write a program to print the table of 9
+num = 9
+counter = 1
+
+#Example on While Loop Example On Tables 
+while counter<=10: # Either True or False
+    ans = num * counter
+    print(num,' * ',counter,' = ',ans)
+    counter = counter + 1
+
+# What will happen if counter not incremented??
+#some infinite while loop example
+while counter<=10: # Either True or False
+    ans = num * counter
+    print(num,' * ',counter,' = ',ans)
+    #counter = counter + 1
+# 9  *  1  =  9 -> 
+
+#some infinite while loop example
+# What will happen?
+while True:
+    print("iNeuron")
