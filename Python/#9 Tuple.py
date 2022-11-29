afd168237af86c11e9300725c21bfbf877b03eaa
RAGHUL R <118167519+Raghulrook@users.noreply.github.com>
2022-11-29 13:23:09 +0530

    Create #9 Tuple.py

---
+++ Python/#9 Tuple.py
@@ -0,0 +1,20 @@
+# See how tuple works 
+#Immutable object
+#empty tuple
+t1 = ()
+#by adding () itself it known as tuple and can't add or remove from the element in the index 
+#failed assignment and append
+#t1.append(5)
+#t1[0] = 3
+
+t2 = (20,30,40,50)
+print(len(t2))
+
+# indexing also works for tuple
+# access 3rd element from tuple
+
+print(t2[2])
+
+# Indexing works
+# Slicing works
+# Iteration also works
